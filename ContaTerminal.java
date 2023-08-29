f8cc6085476ca236c4d7138f7f3dbff03e2fbf1b
CHRISTIAN SPERB <135705689+chrissperb@users.noreply.github.com>
2023-08-29 08:39:51 -0300

    Update ContaTerminal.java
    Aprimoramento do código, criando um método para apresentação das variáveis inseridas. Inserção de comentários.

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -5,6 +5,7 @@ public class ContaTerminal {
 
         Scanner scanner = new Scanner(System.in);
 
+        // Cria o microambiente de trabalho no terminal
         System.out.println("***************************************************");
         System.out.println("           Bem vindo ao DIO Home Banking           ");
         System.out.println("***************************************************");
@@ -12,17 +13,27 @@ public class ContaTerminal {
         System.out.println("Este é o sistema de cadastramento de novos clientes.");
         System.out.println(" ");
 
+        // Captura o nome do cliente
         System.out.print("Digite o seu nome completo: ");
             String nome = "MARIO ANDRADE";
             System.out.println(nome);
 
+        // Captura o valor do depósito inicial
         System.out.print("Qual o valor do depósito inicial? ");
             Double saldo = 237.48;
             System.out.println(saldo);
 
+        // Criação da conta com agência e número
         String agencia = "067-8";
         int conta = 1021;
 
-        System.out.println("Olá, " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta nº " + conta + " e seu saldo de R$" + saldo + " já está disponível para saque.");
+        exibirInformacoesConta(nome, agencia, conta, saldo);
+    }        
+    // TODO: verificar boas práticas utilizadas pela equipe dev Santander
+    // Método para exibir informações da conta
+    public static void exibirInformacoesConta(String nome, String agencia, int conta, double saldo) {
+        System.out.println(" ");
+        System.out.println("Olá, " + nome + ", obrigado por criar uma conta em nosso banco,");
+        System.out.println("sua agência é " + agencia + ", conta nº " + conta + " e seu saldo de R$" + saldo + " já está disponível para saque.");
     }
 }
